774ab34b4016d05885368535bc41867672d78597
Jason Avila-Soria <avilasoriaj22@students.ecu.edu>
2024-11-07 16:39:10 -0500

    Added shutting down functionality to main server file + test

--- Code/main.py
+++ Code/main.py
@@ -3,6 +3,9 @@ import threading
 import logging
 import utils
 import rsa
+import signal #stopping with CTRL + C
+import sys # ^
+import select # ^
 
 # Set up logging
 logging.basicConfig(
@@ -47,91 +50,97 @@ def handle_client(conn, addr):
         conn.close()
         return -1
     
-    while connected:
-        msg_length_data = conn.recv(HEADER)
-        msg_length = msg_length_data.decode(FORMAT).strip()  
+    while connected and not shutdown_flag.is_set():  # if server closed then kill thread. need to add handling for client on sudden connection loss if server dies randomly.
         
-        logging.info(f"msg_length is {msg_length}")
-        if msg_length and msg_length.isdigit():
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length) # REMOVE decode here since its RSAed
-            #logging.info(f"inside msg_length if and statement where msg is {msg}")
-            if not msg:
-                logging.info("No message")
-                continue
-            
-            # Decode first:
-            msg = rsa.decrypt(msg, private_key).decode(FORMAT)
-            
-            ###Check message type
-            if DISCONNECT_MESSAGE in msg:
-                logging.info("got disconnect message, breaking from loop")
-                break
-            ###Logging in stuff here
-            if not LOGGED_IN:
-                if CREATE_ACC in msg: #if u wanna create acc
-                    with database_lock:
-                        _, new_user, new_pass, new_pass2 = msg.split(',')
-                        message = utils.add_account(new_user, new_pass, new_pass2)
-                    conn.send(message.encode(FORMAT))
-                    conn.close()
-                    print("Exiting thread.")
-                    return
-                elif msg != LOGIN_MESSAGE: #if you sent something weird like tryin to access server functions without being logged in...
-                    logging.error("Not logged in.")
-                    break
-                else:
-                    #you did send login message but not yet logged in
-                    # Ask client for their username and password
-                    try:
-                        conn.send("Enter your username: ".encode(FORMAT))
-                        username = rsa.decrypt(conn.recv(1024), private_key).decode(FORMAT)
-                        conn.send("Enter your password: ".encode(FORMAT))
-                        password =rsa.decrypt(conn.recv(1024), private_key).decode(FORMAT)
-                        if not utils.authenticate(username, password):
-                            print(f"Invalid Credentials or user {username} does not exist.")
-                            break
-                        conn.send(AUTH_RESPONSE.encode(FORMAT))
-                        LOGGED_IN = True
-                        connected_clients[username] = {
-                            'ip': addr[0],
-                            'port': addr[1],
-                            'connection': conn
-                        }
-                        print(f"{username} connected.")
+        ready = select.select([conn], [], [], 1.0)  # 1 second timeout
+        if ready[0]:
+            try:
+                msg_length_data = conn.recv(HEADER)
+                msg_length = msg_length_data.decode(FORMAT).strip()  
+                
+                logging.info(f"msg_length is {msg_length}")
+                if msg_length and msg_length.isdigit():
+                    msg_length = int(msg_length)
+                    msg = conn.recv(msg_length) # REMOVE decode here since its RSAed
+                    #logging.info(f"inside msg_length if and statement where msg is {msg}")
+                    if not msg:
+                        logging.info("No message")
                         continue
-                    except Exception as e:
-                        logging.error(f"Unexpected error: {e}")
-                        logging.error("Quitting.")
+                    
+                    # Decode first:
+                    msg = rsa.decrypt(msg, private_key).decode(FORMAT)
+                    
+                    ###Check message type
+                    if DISCONNECT_MESSAGE in msg:
+                        logging.info("got disconnect message, breaking from loop")
                         break
-            #If here then then client still connected
-            if HISTORY_MESSAGE in msg:
-                logging.info("getting history")
-                with database_lock:
-                    history = utils.get_chat_history(msg)
-                _, curr_user, target_user = msg.split(',')
-                if history is None:
-                    #no previous chat history
-                    continue
-                for dm in history:
-                    conn.send(f"{dm['owner']}: {dm['contents']}\n".encode(FORMAT))
-                connected_clients[username]['chat_area']  = True #only update this here since i know from client code only in chat area if history message between user and sender
-            if ALL_CHATS in msg:
-                _, username = msg.split(',')
-                chats = utils.get_chats_only(username)
-                logging.info(f"Sent chat history of: {chats}")
-                if chats is None:
-                    conn.send("None".encode(FORMAT))
-                conn.send(str(chats).encode(FORMAT))
-            if MSG in msg:
-                #append chat data
-                with username_lock:
-                    utils.add_chat(curr_user, target_user, msg)
-                if connected_clients[username]['chat_area']:
-                    utils.send_update_target(curr_user, target_user, msg, connected_clients) #dont need otherwise since they get chat history automagically anyways at beginning of opening chat.
-            #Logging purposes
-            if msg:
-                logging.info(f"Got message: {msg}")
+                    ###Logging in stuff here
+                    if not LOGGED_IN:
+                        if CREATE_ACC in msg: #if u wanna create acc
+                            with database_lock:
+                                _, new_user, new_pass, new_pass2 = msg.split(',')
+                                message = utils.add_account(new_user, new_pass, new_pass2)
+                            conn.send(message.encode(FORMAT))
+                            conn.close()
+                            print("Exiting thread.")
+                            return
+                        elif msg != LOGIN_MESSAGE: #if you sent something weird like tryin to access server functions without being logged in...
+                            logging.error("Not logged in.")
+                            break
+                        else:
+                            #you did send login message but not yet logged in
+                            # Ask client for their username and password
+                            try:
+                                conn.send("Enter your username: ".encode(FORMAT))
+                                username = rsa.decrypt(conn.recv(1024), private_key).decode(FORMAT)
+                                conn.send("Enter your password: ".encode(FORMAT))
+                                password =rsa.decrypt(conn.recv(1024), private_key).decode(FORMAT)
+                                if not utils.authenticate(username, password):
+                                    print(f"Invalid Credentials or user {username} does not exist.")
+                                    break
+                                conn.send(AUTH_RESPONSE.encode(FORMAT))
+                                LOGGED_IN = True
+                                connected_clients[username] = {
+                                    'ip': addr[0],
+                                    'port': addr[1],
+                                    'connection': conn
+                                }
+                                print(f"{username} connected.")
+                                continue
+                            except Exception as e:
+                                logging.error(f"Unexpected error: {e}")
+                                logging.error("Quitting.")
+                                break
+                    #If here then then client still connected
+                    if HISTORY_MESSAGE in msg:
+                        logging.info("getting history")
+                        with database_lock:
+                            history = utils.get_chat_history(msg)
+                        _, curr_user, target_user = msg.split(',')
+                        if history is None:
+                            #no previous chat history
+                            continue
+                        for dm in history:
+                            conn.send(f"{dm['owner']}: {dm['contents']}\n".encode(FORMAT))
+                        connected_clients[username]['chat_area']  = True #only update this here since i know from client code only in chat area if history message between user and sender
+                    if ALL_CHATS in msg:
+                        _, username = msg.split(',')
+                        chats = utils.get_chats_only(username)
+                        logging.info(f"Sent chat history of: {chats}")
+                        if chats is None:
+                            conn.send("None".encode(FORMAT))
+                        conn.send(str(chats).encode(FORMAT))
+                    if MSG in msg:
+                        #append chat data
+                        with username_lock:
+                            utils.add_chat(curr_user, target_user, msg)
+                        if connected_clients[username]['chat_area']:
+                            utils.send_update_target(curr_user, target_user, msg, connected_clients) #dont need otherwise since they get chat history automagically anyways at beginning of opening chat.
+                    #Logging purposes
+                    if msg:
+                        logging.info(f"Got message: {msg}")
+            except socket.error: #if error (like client closes)
+                break
 
     logging.info("Closing socket connection")
     conn.close()
@@ -140,17 +149,52 @@ def handle_client(conn, addr):
     logging.info("Returning...")
     return
 
+def signal_handler(sig, frame):
+    """Handle CTRL+C shutdown"""
+    print("\n[INFO] Server is shutting down...")
+    shutdown_flag.set()  # Set the shutdown flag
+    
+    # Close all client connections
+    for client_info in connected_clients.values():
+        try:
+            client_info['connection'].close()
+        except:
+            pass
+    connected_clients.clear()
+    
+    # Create a dummy connection to unblock accept()
+    try:
+        dummy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        dummy.connect((server_ip, server_port))
+        dummy.close()
+    except:
+        pass
 
 connected_clients = {} # For storing active clients.
+shutdown_flag = threading.Event()  # global flag to shutdown
 
 def start():
     print(f"[LISTENING] Server is listening on {server_ip}")
     server.listen()
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+    signal.signal(signal.SIGINT, signal_handler) #Added for stopping server with CTRL + C
+    while not shutdown_flag.is_set():
+        try:
+            # Use select to make accept() interruptible
+            ready = select.select([server], [], [], 1.0)  # 1 second timeout
+            if ready[0]:
+                conn, addr = server.accept()
+                if shutdown_flag.is_set():  # check flag again after timeout
+                    conn.close()
+                    break
+                thread = threading.Thread(target=handle_client, args=(conn, addr))
+                thread.start()
+                print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        except Exception as e:
+            if not shutdown_flag.is_set():  # Only log if not shutting down
+                print(f"[ERROR] {e}")
+            break
+    
+    print("[INFO] Server shutdown complete")
 
 print("[STARTING] server is starting...")
 start()
--- tests/test_server.py
+++ tests/test_server.py
@@ -16,9 +16,20 @@ def start_server():
         time.sleep(1)
         yield buf  # gives output to the test function
 
-def test_server_startup(start_server):
+def test_server_startup_and_shutdown(start_server):
     output = start_server.getvalue()  # Retrieve captured output
-    
-    # Verify that the expected startup messages are present
-    assert "[STARTING] server is starting..." in output, "Server should print start message."
+
+    # Verify that the server starts correctly
+    assert "[STARTING] server is starting..." in output, "Server should print the start message."
     assert "[LISTENING] Server is listening on" in output, "Server should indicate it is listening."
+
+    # Simulate a shutdown by raising a KeyboardInterrupt (this is what `CTRL+C` would trigger)
+    # This should cause the server to cleanly shut down
+    try:
+        raise KeyboardInterrupt
+    except KeyboardInterrupt:
+        # The server should print shutdown logs
+        time.sleep(1)  # Wait for server shutdown logs to be captured
+        shutdown_output = start_server.getvalue()
+        assert "[INFO] Server is shutting down..." in shutdown_output, "Server should print shutting down message."
+        assert "[INFO] Server shutdown complete" in shutdown_output, "Server should indicate it has completed shutdown."
